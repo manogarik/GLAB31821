d80914ae4d6c25d864ba48de5b88836a764f66f3
Manogari Kumaraguru <manogarikumaraguru@gmail.com>
2025-04-08 23:10:21 -0400

    final

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 5500;
+//Part 2 using middleware
 //middleware to encode the url
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,6 +25,18 @@ app.post('/submit',(req,res)=>
 {
     const { name, email } = req.body;
     res.send(`Hi! ${name} ! You are successfully registered`);
+})
+//download an image
+app.get('/download',(req,res)=>
+{
+    const filepath =path.join(__dirname,'images','dog.jpg') ;
+    res.download(filepath,(err)=>
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+    });
 })
 app.listen(5500,()=>{
     console.log('Server running on http://localhost:5500 ')
